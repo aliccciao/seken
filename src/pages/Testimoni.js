4ec67db5746c23801ddb47bb1ec2f750194a67a4
aliccciao <alicia.utomo@gmail.com>
2023-09-16 18:11:13 +0700

    DONE: Testimoni

--- src/pages/Testimoni.js
+++ src/pages/Testimoni.js
@@ -1,12 +1,12 @@
 const Testimoni = () => {
     return (
         <div className="Testimoni">
-            <div className="min-w-screen min-h-screen flex items-center justify-center py-5">
-                <div className="w-full border-b border-gray-200 px-5 py-16 md:py-24 text-gray-800">
+            <div className="min-w-screen bg-dark min-h-screen flex items-center justify-center py-5">
+                <div className="w-full px-5 py-16 md:py-24 text-gray-800">
                     <div className="w-full max-w-6xl mx-auto">
                         <div className="text-center max-w-xl mx-auto">
-                            <h1 className="text-6xl md:text-7xl font-extrabold mb-5 text-dark">Apa kata mereka?</h1>
-                            <h3 className="text-xl mb-5 text-primary">Cari tahu bagaimana SEKEN telah membantu pembeli dan penjual di seluruh Indonesia!</h3>
+                            <h1 className="text-6xl md:text-7xl font-extrabold mb-5 text-lime">Apa kata mereka?</h1>
+                            <h3 className="text-xl mb-5 text-white">Cari tahu bagaimana SEKEN telah membantu pembeli dan penjual di seluruh Indonesia!</h3>
                             <div className="text-center mb-10">
                                 <span className="inline-block w-40 h-1 rounded-full bg-dark"></span>
                             </div>
@@ -19,12 +19,12 @@ const Testimoni = () => {
                                             <img src="https://i.pravatar.cc/100?img=1" alt="" />
                                         </div>
                                         <div className="flex-grow pl-3">
-                                            <h6 className="font-bold text-sm uppercase text-gray-600">Astrid Alison</h6>
+                                            <h6 className="font-bold text-sm uppercase text-dark">Astrid Alison</h6>
                                             <p className="text-sm">Mahasiswa - 20 tahun</p>
                                         </div>
                                     </div>
                                     <div className="w-full">
-                                        <p className="text-sm leading-tight"><span className="text-lg leading-none italic font-bold text-gray-400 mr-1">"</span>Selama ini aku bingung banget nyari tempat yang cocok buat jual baju-baju bekas aku. Tapi setelah ada <b>SEKEN</b> semuanya jadi gampang banget!<span className="text-lg leading-none italic font-bold text-gray-400 ml-1">"</span></p>
+                                        <p className="text-sm leading-tight"><span className="text-lg leading-none italic font-bold text-gray-400 mr-1">"</span>Selama ini aku bingung banget nyari tempat yang cocok buat jual baju-baju bekas aku. Tapi setelah ada <b className="underline decoration-20 decoration-lime">SEKEN</b> semuanya jadi gampang banget!<span className="text-lg leading-none italic font-bold text-gray-400 ml-1">"</span></p>
                                     </div>
                                 </div>
                                 <div className="w-full mx-auto rounded-lg bg-white border border-gray-200 p-5 text-gray-800 font-normal mb-6">
@@ -33,12 +33,12 @@ const Testimoni = () => {
                                             <img src="https://i.pravatar.cc/100?img=5" alt="" />
                                         </div>
                                         <div className="flex-grow pl-3">
-                                            <h6 className="font-bold text-sm uppercase text-gray-600">Michelle Widjaja</h6>
+                                            <h6 className="font-bold text-sm uppercase text-dark">Michelle Widjaja</h6>
                                             <p className="text-sm">Karyawan - 23 tahun</p>
                                         </div>
                                     </div>
                                     <div className="w-full">
-                                        <p className="text-sm leading-tight"><span className="text-lg leading-none italic font-bold text-gray-400 mr-1">"</span>Suka banget bisa nemu aplikasi kaya <b>SEKEN</b> soalnya ga perlu takut ditipu sama seller soalnya ada jaminan keamanan transaksi.<span className="text-lg leading-none italic font-bold text-gray-400 ml-1">"</span></p>
+                                        <p className="text-sm leading-tight"><span className="text-lg leading-none italic font-bold text-gray-400 mr-1">"</span>Suka banget bisa nemu aplikasi kaya <b className="underline decoration-20 decoration-lime">SEKEN</b> soalnya ga perlu takut ditipu sama seller soalnya ada jaminan keamanan transaksi.<span className="text-lg leading-none italic font-bold text-gray-400 ml-1">"</span></p>
                                     </div>
                                 </div>
                             </div>
@@ -49,12 +49,12 @@ const Testimoni = () => {
                                             <img src="https://i.pravatar.cc/100?img=8" alt="" />
                                         </div>
                                         <div className="flex-grow pl-3">
-                                            <h6 className="font-bold text-sm uppercase text-gray-600">Budi Budiman</h6>
+                                            <h6 className="font-bold text-sm uppercase text-dark">Budi Budiman</h6>
                                             <p className="text-sm">Mahasiswa - 19 tahun</p>
                                         </div>
                                     </div>
                                     <div className="w-full">
-                                        <p className="text-sm leading-tight"><span className="text-lg leading-none italic font-bold text-gray-400 mr-1">"</span>Gue suka banget sama <b>SEKEN</b> soalnya bisa langsung kirim pesan ke seller buat nego harga & tanya-tanya tentang pengiriman.<span className="text-lg leading-none italic font-bold text-gray-400 ml-1">"</span></p>
+                                        <p className="text-sm leading-tight"><span className="text-lg leading-none italic font-bold text-gray-400 mr-1">"</span>Gue suka banget sama <b className="underline decoration-20 decoration-lime">SEKEN</b> soalnya bisa langsung kirim pesan ke seller buat nego harga & tanya-tanya tentang pengiriman.<span className="text-lg leading-none italic font-bold text-gray-400 ml-1">"</span></p>
                                     </div>
                                 </div>
                                 <div className="w-full mx-auto rounded-lg bg-white border border-gray-200 p-5 text-gray-800 font-normal mb-6">
@@ -63,12 +63,12 @@ const Testimoni = () => {
                                             <img src="https://i.pravatar.cc/100?img=10" alt="" />
                                         </div>
                                         <div className="flex-grow pl-3">
-                                            <h6 className="font-bold text-sm uppercase text-gray-600">Rahma Susanti</h6>
+                                            <h6 className="font-bold text-sm uppercase text-dark">Rahma Susanti</h6>
                                             <p className="text-sm">Pelajar - 17 tahun</p>
                                         </div>
                                     </div>
                                     <div className="w-full">
-                                        <p className="text-sm leading-tight"><span className="text-lg leading-none italic font-bold text-gray-400 mr-1">"</span>Sekarang kalau mau jual baju tinggal ke <b>SEKEN</b> aja, ga perlu ribet!<span className="text-lg leading-none italic font-bold text-gray-400 ml-1">"</span></p>
+                                        <p className="text-sm leading-tight"><span className="text-lg leading-none italic font-bold text-gray-400 mr-1">"</span>Sekarang kalau mau jual baju tinggal ke <b className="underline decoration-20 decoration-lime">SEKEN</b> aja, ga perlu ribet!<span className="text-lg leading-none italic font-bold text-gray-400 ml-1">"</span></p>
                                     </div>
                                 </div>
                             </div>
@@ -79,12 +79,12 @@ const Testimoni = () => {
                                             <img src="https://i.pravatar.cc/100?img=7" alt="" />
                                         </div>
                                         <div className="flex-grow pl-3">
-                                            <h6 className="font-bold text-sm uppercase text-gray-600">Lewis Hamilton</h6>
+                                            <h6 className="font-bold text-sm uppercase text-dark">Lewis Hamilton</h6>
                                             <p className="text-sm">Karyawan - 34 tahun</p>
                                         </div>
                                     </div>
                                     <div className="w-full">
-                                        <p className="text-sm leading-tight"><span className="text-lg leading-none italic font-bold text-gray-400 mr-1">"</span>Sangat memuaskan berbelanja di <b>SEKEN</b>. Tidak hanya membantu saya menghemat uang, tetapi juga membantu dalam berkontribusi dalam menjaga lingkungan dengan membeli barang bekas.<span className="text-lg leading-none italic font-bold text-gray-400 ml-1">"</span></p>
+                                        <p className="text-sm leading-tight"><span className="text-lg leading-none italic font-bold text-gray-400 mr-1">"</span>Sangat memuaskan berbelanja di <b className="underline decoration-20 decoration-lime">SEKEN</b>. Tidak hanya membantu saya menghemat uang, tetapi juga membantu dalam berkontribusi dalam menjaga lingkungan dengan membeli barang bekas.<span className="text-lg leading-none italic font-bold text-gray-400 ml-1">"</span></p>
                                     </div>
                                 </div>
                                 <div className="w-full mx-auto rounded-lg bg-white border border-gray-200 p-5 text-gray-800 font-normal mb-6">
@@ -93,7 +93,7 @@ const Testimoni = () => {
                                             <img src="https://i.pravatar.cc/100?img=11" alt="" />
                                         </div>
                                         <div className="flex-grow pl-3">
-                                            <h6 className="font-bold text-sm uppercase text-gray-600">Agus Bambang Perkasa</h6>
+                                            <h6 className="font-bold text-sm uppercase text-dark">Agus Bambang Perkasa</h6>
                                             <p className="text-sm">Karyawan - 54 tahun</p>
                                         </div>
                                     </div>
@@ -104,7 +104,7 @@ const Testimoni = () => {
                             </div>
                         </div>
 
-                        <h3 className="text-m mb-5 text-grey">Dan masih banyak lagi ...</h3>
+                        <h3 className="text-m mb-5 text-white">Dan masih banyak lagi ...</h3>
                     </div>
                 </div>
             </div>
